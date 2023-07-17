744b60fd60048894500858833dfbb1e1220558e8
More <115424742+moreg0mez@users.noreply.github.com>
2023-07-17 02:40:40 -0300

    Ultimas correcciones

--- Scripts/Procesar.sh
+++ Scripts/Procesar.sh
@@ -3,15 +3,15 @@
 #voy a la carpeta donde esta imagenes_tp
 cd ..
 
-#Verifico que exista sino tiro error
+#Verifico que exista, si no retorno un mensaje de error.
 if [ -e imagenes_tp ]; then
    echo "La carpeta de imagenes existe... procesando imagenes..."
 else
-   echo "ERROR: La carpeta de imagenes NO EXISTE. 1ro debe ejecutar Generar.sh"
+   echo "ERROR: La carpeta de imagenes NO EXISTE. Antes debe ejecutar Generar.sh"
 fi
 
-#Pongo que no hace falte incluir argumentos, el script solo necesita ser
-#ejecutado y procesa las imaganes
+#Aclaro que no hace falte incluir argumentos, el script solo necesita ser
+#ejecutado para procesar las imagenes
 [ "$#" -ne 0 ] && echo "Ejecute el script sin argumentos" && exit 1
 
 #Me aseguro que esta el imagemagick
@@ -25,7 +25,7 @@ for i in ./imagenes_tp/* # El wildcard toma todos los nombres
 do                       # de archivo, luego basename deja SOLO el nombre del archivo
    NOMBRE=$(basename "$i")
                                             # =~ compara el Nombre con la expresion regular para verificar
-   if [[ "$NOMBRE" =~ ^[A-Z][a-z]+ ]]; then #que empiece com mayuscula y siga con minusculas
+   if [[ "$NOMBRE" =~ ^[A-Z][a-z]+ ]]; then #que empiece con mayuscula y siga con minusculas
       convert "./imagenes_tp/$NOMBRE" -gravity center -resize 512x512+0+0 -extent 512x512 "./imagenes_tp_procesadas/$NOMBRE"
       echo "Imagen procesada, aguarde..."
    else continue
