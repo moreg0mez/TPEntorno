94d893e367b9d14d641e8439e04805975a76a4ce
More <115424742+moreg0mez@users.noreply.github.com>
2023-07-16 22:45:37 -0300

    Corrección de comentarios

--- Scripts/Procesar.sh
+++ Scripts/Procesar.sh
@@ -7,7 +7,7 @@ cd ..
 if [ -e imagenes_tp ]; then
    echo "La carpeta de imagenes existe... procesando imagenes..."
 else
-   echo "ERROR: La carpeta de imaganes NO EXISTE. 1ro debe ejecutar Generar.sh"
+   echo "ERROR: La carpeta de imagenes NO EXISTE. 1ro debe ejecutar Generar.sh"
 fi
 
 #Pongo que no hace falte incluir argumentos, el script solo necesita ser
@@ -25,7 +25,7 @@ for i in ./imagenes_tp/* # El wildcard toma todos los nombres
 do                       # de archivo, luego basename deja SOLO el nombre del archivo
    NOMBRE=$(basename "$i")
                                             # =~ compara el Nombre con la expresion regular para verificar
-   if [[ "$NOMBRE" =~ ^[A-Z][a-z]+ ]]; then #que empieze com mayuscula y siga con minusculas
+   if [[ "$NOMBRE" =~ ^[A-Z][a-z]+ ]]; then #que empiece com mayuscula y siga con minusculas
       convert "./imagenes_tp/$NOMBRE" -gravity center -resize 512x512+0+0 -extent 512x512 "./imagenes_tp_procesadas/$NOMBRE"
       echo "Imagen procesada, aguarde..."
    else continue
@@ -33,7 +33,7 @@ do                       # de archivo, luego basename deja SOLO el nombre del ar
 
 done
 
-echo "Todas las imaganes fueron procesadas y se guardaron en imaganes_tp_procesadas"
+echo "Todas las imagenes fueron procesadas y guardadas en imagenes_tp_procesadas"
 
 exit 0
 
