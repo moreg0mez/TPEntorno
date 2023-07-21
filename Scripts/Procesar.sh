0c96cef96524b8370adb63bc242092593f14c90c
moreg0mez <moregomez1812@gmail.com>
2023-07-21 10:41:44 -0300

    Procesar.sh
    VERSION FINAL

--- Scripts/Procesar.sh
+++ Scripts/Procesar.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
-#voy a la carpeta donde esta imagenes_tp
+#Navegamos hasta la carpeta donde está imagenes_tp
 cd ..
 
-#Verifico que exista sino tiro error
+#En caso de que se intente ejecuar dos veces el script Procesar.sh, se retorna error
+[ -e imagenes_tp_procesadas ] && echo "ERROR: La carpeta imagenes_tp_procesadas ya existe, continue con la opcion 4)" && exit 1
+
+#Verificsmod que exista, si no retorno un mensaje de error.
 if [ -e imagenes_tp ]; then
    echo "La carpeta de imagenes existe... procesando imagenes..."
 else
-   echo "ERROR: La carpeta de imaganes NO EXISTE. 1ro debe ejecutar Generar.sh"
+   echo -e "ERROR: La carpeta de imagenes NO EXISTE. Antes ejecute 1) o 2)\n"
+   exit 1
 fi
 
-#Pongo que no hace falte incluir argumentos, el script solo necesita ser
-#ejecutado y procesa las imaganes
+#Aclaramos que no es necesario incluir argumentos, el script solo necesita ser
+#ejecutado para procesar las imagenes
 [ "$#" -ne 0 ] && echo "Ejecute el script sin argumentos" && exit 1
 
-#Me aseguro que esta el imagemagick
-sudo apt-get install imagemagick
-
-#Creo la nueva carpeta en donde se guardaran las imagenes procesadas
+#Creo la nueva carpeta en la cual se guardarán las imagenes procesadas
 mkdir imagenes_tp_procesadas
 
 #Se inicia un loop for para iterar sobre las imagenes de imagenes_tp
@@ -25,7 +26,7 @@ for i in ./imagenes_tp/* # El wildcard toma todos los nombres
 do                       # de archivo, luego basename deja SOLO el nombre del archivo
    NOMBRE=$(basename "$i")
                                             # =~ compara el Nombre con la expresion regular para verificar
-   if [[ "$NOMBRE" =~ ^[A-Z][a-z]+ ]]; then #que empieze com mayuscula y siga con minusculas
+   if [[ "$NOMBRE" =~ ^[A-ZÑ][a-zñ]+ ]]; then #que empiece con mayuscula y siga con minusculas.
       convert "./imagenes_tp/$NOMBRE" -gravity center -resize 512x512+0+0 -extent 512x512 "./imagenes_tp_procesadas/$NOMBRE"
       echo "Imagen procesada, aguarde..."
    else continue
@@ -33,13 +34,14 @@ do                       # de archivo, luego basename deja SOLO el nombre del ar
 
 done
 
-echo "Todas las imaganes fueron procesadas y se guardaron en imaganes_tp_procesadas"
+echo "Todas las imagenes fueron procesadas y guardadas en imagenes_tp_procesadas"
+
+#Imprimimos menu
+echo -e "Lista de opciones:" 
+echo -e "1) Generar.sh" 
+echo -e "2) Descomprimir.sh"
+echo -e "3) Procesar.sh"
+echo -e "4) Comprimir.sh"
+echo -e "5) Quit" 
 
 exit 0
-
-
-
-
-
-
-
